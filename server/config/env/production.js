acf3325cdeb0443d55eff6b4bd481a3e2b0a17f5
Donghyun Go <godong9@gmail.com>
2015-07-01 22:09:39 +0900

    Update

---
+++ server/config/env/production.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+    env: 'production',
+    host: 'localhost',
+    port: 7000,
+    secret: 'study_secret',
+    mongo: {
+        uri: 'mongodb://localhost:27017/',
+        db: 'study',
+        username: '',
+        password: ''
+    }
+
+};
